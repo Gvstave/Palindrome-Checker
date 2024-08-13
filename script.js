bcf3fabcf4564a7bb215775de35a668450b6245e
Gvstave <139679793+Gvstave@users.noreply.github.com>
2024-08-13 21:50:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const text_input = document.querySelector('#text-input')
 const check_btn = document.querySelector('#check-btn')
 const result = document.querySelector('#result')
-
 check_btn.addEventListener('click', function(){
     const input_value = text_input.value
-        const input_remove = input_value.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
-            const input_reverse = input_remove.split("").reverse().join("")
-                
-                    if(input_value === ""){
-                        alert("Please input a value.")
-                          }
-                              else if(input_remove === input_reverse){
-                                      result.innerHTML = `${input_value} is a palindrome`
-                                          }
-                                              else{
-                                                      result.innerHTML = `${input_value} is not a palindrome`
-                                                          }
-                                                            
-                                                            })
+    const input_remove = input_value.toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+    const input_reverse = input_remove.split("").reverse().join("")
+    
+    if(input_value === ""){
+    alert("Please input a value.")
+  }
+    else if(input_remove === input_reverse){
+        result.innerHTML = `${input_value} (${input_reverse}) is a palindrome`
+    }
+    else{
+        result.innerHTML = `${input_value} (${input_reverse}) is not a palindrome`
+        setTimeout(()=> {
+            alert("Palindrome example: eye, racecar, RacEcar, -e_yE etc")
+        }, 400)
+    }
+  
+})
